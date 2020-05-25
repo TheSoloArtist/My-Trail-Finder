080838ba1994a36dfa5db93a40a79b5607fcf1aa
Mark F <the.mark.fullton@gmail.com>
2020-05-25 11:23:42 -0700

    Set up bing maps; originally boots with LA

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,31 +2,8 @@
  * added until the coordinates are retrieved from
  * BingMaps
  * */
-var retrievedQuery = false;
 var latitude = null;
 var longitude = null;
-
-/* Function takes in coordinates and creates the
- * BingMaps based off them.
- */
-function getMap(searchGeocode) {
-  console.log("Ran getMap function");
-  var centerText =
-    "new Microsoft.Maps.Location(" +
-    searchGeocode[0] +
-    "," +
-    searchGeocode[1] +
-    ")";
-
-  console.log(centerText);
-
-  var map = new Microsoft.Maps.Map("#parkMap", {
-    credentials: config.BING_MAPS_API,
-    center: centerText,
-    mapTypeId: Microsoft.Maps.MapTypeId.aerial,
-    zoom: 10,
-  });
-}
 
 /* This takes in the inputted city and state and
  * sends it to Bing Maps to get the coordinates.
@@ -50,10 +27,12 @@ function geocode(city, state) {
     latitude = geocode[0];
     longitude = geocode[1];
     restaurantSearch();
-    console.log(geocode);
-
-    retrievedQuery = true;
-    return geocode;
+    map.setView({
+      mapTypeId: Microsoft.Maps.MapTypeId.aerial,
+      center: new Microsoft.Maps.Location(geocode[0], geocode[1]),
+      zoom: 10,
+    });
+    console.log("Attempted to change map");
   });
 }
 
@@ -76,53 +55,35 @@ $(document).ready(function () {
 
     // Sets coordinates of location to searchGeocode
     var searchGeocode = geocode(city, state);
-
-    /* This ensures the program does not run
-     * getMap function until the geocode has loaded
-     */
-
-    if (retrievedQuery === true) {
-      retrievedQuery = false;
-      console.log(searchGeocode);
-      getMap(searchGeocode);
-    }
-
-    var fruit = ["kiwi", "orange"];
-    console.log(fruit);
-
-
   });
-
 });
 
 function restaurantSearch() {
   var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": `https://tripadvisor1.p.rapidapi.com/restaurants/list-by-latlng?limit=3&currency=USD&distance=2&lunit=km&lang=en_US&latitude=${latitude}&longitude=${longitude}`,
-    "method": "GET",
-    "headers": {
+    async: true,
+    crossDomain: true,
+    url: `https://tripadvisor1.p.rapidapi.com/restaurants/list-by-latlng?limit=3&currency=USD&distance=2&lunit=km&lang=en_US&latitude=${latitude}&longitude=${longitude}`,
+    method: "GET",
+    headers: {
       "x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
-      "x-rapidapi-key": "adb75d27cfmshe591e88122b81ffp14a2e1jsnaed0acc3fb97"
-    }
-  }
+      "x-rapidapi-key": "adb75d27cfmshe591e88122b81ffp14a2e1jsnaed0acc3fb97",
+    },
+  };
 
   $.ajax(settings).done(function (response) {
     console.log(response);
-    let myRes = response.data
+    let myRes = response.data;
     for (var i = 0; i < myRes.length; i++) {
-      var wrapper = $("<div>")
-      wrapper.attr("class", "Pleasechangeme")
-      var pTag = $("<p>")
-      pTag.text(myRes[i].name)
-      var phone = $("<p>")
-      phone.text("Tell: " + myRes[i].phone)
+      var wrapper = $("<div>");
+      wrapper.attr("class", "Pleasechangeme");
+      var pTag = $("<p>");
+      pTag.text(myRes[i].name);
+      var phone = $("<p>");
+      phone.text("Tell: " + myRes[i].phone);
 
-      // after creating all the tags we have to append them to the wrapper and then append the wrapper to the div with the class of restuarant 
-      wrapper.append(pTag, phone)
-      $(".restaurant").append(wrapper)
+      // after creating all the tags we have to append them to the wrapper and then append the wrapper to the div with the class of restuarant
+      wrapper.append(pTag, phone);
+      $(".restaurant").append(wrapper);
     }
-
   });
-
 }
