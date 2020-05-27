e52f66577da6e9ed7561373b4c05b697cc4f69d0
Juliana Hernandez <juliana88h@gmail.com>
2020-05-26 20:46:32 -0700

    added updates to js file

--- assets/js/app.js
+++ assets/js/app.js
@@ -69,13 +69,7 @@ $(document).ready(function () {
     console.log(fruit);
 
 
-    //   var stickItToPage= `<a class="weatherwidget-io" href="https://forecast7.com/en/34d05n118d24/${city}/?unit=us"
-    //   data-label_1="LOS ANGELES" data-label_2="WEATHER" data-days="3" data-theme="kiwi">${city} weather</a>
-    // <script>
-    //   !function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.src = 'https://weatherwidget.io/js/widget.min.js'; fjs.parentNode.insertBefore(js, fjs); } }(document, 'script', 'weatherwidget-io-js');
-    // </script>`
-    // $(".weatherShow").append(stickItToPage)
-    // });
+     
   });
 
 });
